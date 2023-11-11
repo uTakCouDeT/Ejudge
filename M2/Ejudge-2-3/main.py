b0423da642db098bfec19d23c1671cb1f5fb659d
uTakCouDeT <b20030021@gmail.com>
2023-11-11 19:05:43 +0300

    ]

--- M2/Ejudge-2-3/main.py
+++ M2/Ejudge-2-3/main.py
@@ -64,41 +64,35 @@ class AutoCorrect:
 
     @staticmethod
     def __is_dam_lev_distance(s1, s2):
-        len_s1 = len(s1)
-        len_s2 = len(s2)
+        len_s1, len_s2 = len(s1), len(s2)
 
-        # Раннее прерывание, если разница в длине больше 1
         if abs(len_s1 - len_s2) > 1:
             return False
 
-        d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
-
-        for i in range(len_s1 + 1):
-            d[i][0] = i
-        for j in range(len_s2 + 1):
-            d[0][j] = j
+        prev_prev_row = None
+        prev_row = list(range(len_s2 + 1))
+        current_row = [0] * (len_s2 + 1)
 
         for i in range(1, len_s1 + 1):
+            current_row[0] = i
             for j in range(1, len_s2 + 1):
                 cost = 0 if s1[i - 1] == s2[j - 1] else 1
-                d[i][j] = min(d[i - 1][j] + 1,  # удаление
-                              d[i][j - 1] + 1,  # вставка
-                              d[i - 1][j - 1] + cost)  # замена
+                current_row[j] = min(prev_row[j] + 1, current_row[j - 1] + 1, prev_row[j - 1] + cost)
 
-                if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
-                    d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
+                if prev_prev_row and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
+                    current_row[j] = min(current_row[j], prev_prev_row[j - 2] + 1)
 
-            # Раннее прерывание, если минимальное расстояние уже больше 1
-            if min(d[i]) > 1:
+            if min(current_row) > 1:
                 return False
 
-        return True if d[len_s1][len_s2] == 1 else False
+            prev_prev_row = prev_row.copy() if 0 in prev_row else None
+            prev_row, current_row = current_row, [0] * (len_s2 + 1)
+
+        return True if prev_row[len_s2] == 1 else False
 
     # @staticmethod
     # def __dam_lev_distance(s1, s2):
-    #
-    #     len_s1 = len(s1)
-    #     len_s2 = len(s2)
+    #     len_s1, len_s2 = len(s1), len(s2)
     #
     #     d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
     #
