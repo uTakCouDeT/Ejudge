ab4652892eb59df024343f1f61cb3b2b3735a317
uTakCouDeT <b20030021@gmail.com>
2023-11-11 18:17:26 +0300

    ъ

--- M2/Ejudge-2-3/main.py
+++ M2/Ejudge-2-3/main.py
@@ -3,23 +3,21 @@ import sys
 
 class Node:
     def __init__(self):
-        self.children = {}
+        self.children = {}  # ключ - следующая буква, значение ссылка на её ноду
         self.is_end_of_word = False
-        self.value = None
 
 
 class Trie:
     def __init__(self):
-        self.__root = Node()
+        self.__root = Node()  # корень пустое слово, по этому значение хранить не нужно
 
-    def add(self, word, value):
+    def add(self, word):
         node = self.__root
         for char in word.lower():
             if char not in node.children:
                 node.children[char] = Node()
             node = node.children[char]
         node.is_end_of_word = True
-        node.value = value
 
     def search(self, word):
         node = self.__root
@@ -30,28 +28,23 @@ class Trie:
         return node if node.is_end_of_word else None
 
     def get_words(self):
-        # Метод для получения всех слов в Trie
         words = []
-        self.__get_words_helper(self.__root, "", words)
-        return words
-
-    @staticmethod
-    def __get_words_helper(node, current_word, words):
-        stack = [(node, current_word)]
+        stack = [(self.__root, "")]
         while stack:
             current_node, word = stack.pop()
             if current_node.is_end_of_word:
                 words.append(word)
             for char, next_node in current_node.children.items():
                 stack.append((next_node, word + char))
+        return words
 
 
 class AutoCorrect:
     def __init__(self):
         self.__trie = Trie()
 
-    def add_word(self, word, value=None):
-        self.__trie.add(word, value)
+    def add_word(self, word):
+        self.__trie.add(word)
 
     def correct_word(self, word):
         lower_word = word.lower()
@@ -60,9 +53,8 @@ class AutoCorrect:
 
         corrections = []
         for dict_word in self.__trie.get_words():
-            if abs(len(lower_word) - len(dict_word)) <= 1:
-                if self.__dam_lev_distance(lower_word, dict_word) == 1:
-                    corrections.append(dict_word)
+            if self.__is_dam_lev_distance(lower_word, dict_word):
+                corrections.append(dict_word)
 
         if corrections:
             corrections.sort()
@@ -71,14 +63,15 @@ class AutoCorrect:
             return f"{word} -?"
 
     @staticmethod
-    def __dam_lev_distance(s1, s2):
-        if abs(len(s1) - len(s2)) > 1:
-            return 2  # Разница в длине больше 1, расстояние точно > 1
-
+    def __is_dam_lev_distance(s1, s2):
         len_s1 = len(s1)
         len_s2 = len(s2)
 
-        d = [[0 for _ in range(len_s2 + 1)] for _ in range(len_s1 + 1)]
+        # Раннее прерывание, если разница в длине больше 1
+        if abs(len_s1 - len_s2) > 1:
+            return False
+
+        d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
 
         for i in range(len_s1 + 1):
             d[i][0] = i
@@ -95,7 +88,36 @@ class AutoCorrect:
                 if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
                     d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
 
-        return d[len_s1][len_s2]
+            # Раннее прерывание, если минимальное расстояние уже больше 1
+            if min(d[i]) > 1:
+                return False
+
+        return True if d[len_s1][len_s2] == 1 else False
+
+    # @staticmethod
+    # def __dam_lev_distance(s1, s2):
+    #
+    #     len_s1 = len(s1)
+    #     len_s2 = len(s2)
+    #
+    #     d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
+    #
+    #     for i in range(len_s1 + 1):
+    #         d[i][0] = i
+    #     for j in range(len_s2 + 1):
+    #         d[0][j] = j
+    #
+    #     for i in range(1, len_s1 + 1):
+    #         for j in range(1, len_s2 + 1):
+    #             cost = 0 if s1[i - 1] == s2[j - 1] else 1
+    #             d[i][j] = min(d[i - 1][j] + 1,  # удаление
+    #                           d[i][j - 1] + 1,  # вставка
+    #                           d[i - 1][j - 1] + cost)  # замена
+    #
+    #             if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
+    #                 d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
+    #
+    #     return d[len_s1][len_s2]
 
 
 def main():
