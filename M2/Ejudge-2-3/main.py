6ee4687f64a2c332d051cff6e95adb660f05fb63
uTakCouDeT <b20030021@gmail.com>
2023-11-11 21:38:09 +0300

    ъ

--- M2/Ejudge-2-3/main.py
+++ M2/Ejudge-2-3/main.py
@@ -1,69 +1,106 @@
 import sys
 
 
+# class Node:
+#     def __init__(self):
+#         self.children = {}  # ключ - следующая буква, значение ссылка на её ноду
+#         self.is_end_of_word = False
+#
+#
+# class Trie:
+#     def __init__(self):
+#         self.__root = Node()  # корень - всегда пустое слово, по этому его значение хранить не нужно
+#
+#     def add(self, word):
+#         node = self.__root
+#         for char in word.lower():
+#             if char not in node.children:
+#                 node.children[char] = Node()
+#             node = node.children[char]
+#         node.is_end_of_word = True
+#
+#     def search(self, word):
+#         node = self.__root
+#         for char in word.lower():
+#             if char not in node.children:
+#                 return None
+#             node = node.children[char]
+#         return node if node.is_end_of_word else None
+#
+#     def get_words(self):
+#         words = []
+#         stack = [(self.__root, "")]
+#         while stack:
+#             current_node, word = stack.pop()
+#             if current_node.is_end_of_word:
+#                 words.append(word)
+#             for char, next_node in current_node.children.items():
+#                 stack.append((next_node, word + char))
+#         return words
+
+
 class Node:
     def __init__(self):
-        self.children = {}  # ключ - следующая буква, значение ссылка на её ноду
-        self.is_end_of_word = False
+        self.children = {}
+        self.is_word = False
 
 
-class Trie:
+# Сначала сделал через обычное префиксное дерево, а потом перечитал условие и пошел его "сжимать"...
+class RadixTree:
     def __init__(self):
-        self.__root = Node()  # корень пустое слово, по этому значение хранить не нужно
+        self.__root = Node()
 
     def add(self, word):
+        word = word.lower()
         node = self.__root
-        for char in word.lower():
-            if char not in node.children:
-                node.children[char] = Node()
-            node = node.children[char]
-        node.is_end_of_word = True
+        i = 0
+        while i < len(word):
+            match = False
+            for child in node.children:
+                if word[i:].startswith(child):
+                    match = True
+                    node = node.children[child]
+                    i += len(child)
+                    break
+            if not match:
+                new_node = Node()
+                new_node.is_word = True
+                node.children[word[i:]] = new_node
+                break
 
     def search(self, word):
+        word = word.lower()
         node = self.__root
-        for char in word.lower():
-            if char not in node.children:
-                return None
-            node = node.children[char]
-        return node if node.is_end_of_word else None
+        i = 0
+        while i < len(word):
+            found = False
+            for child in node.children:
+                if word[i:].startswith(child):
+                    found = True
+                    node = node.children[child]
+                    i += len(child)
+                    break
+            if not found:
+                return False
+        return node.is_word
 
-    def get_words(self):
-        words = []
+    def get_corrections(self, word):
         stack = [(self.__root, "")]
-        while stack:
-            current_node, word = stack.pop()
-            if current_node.is_end_of_word:
-                words.append(word)
-            for char, next_node in current_node.children.items():
-                stack.append((next_node, word + char))
-        return words
-
-
-class AutoCorrect:
-    def __init__(self):
-        self.__trie = Trie()
-
-    def add_word(self, word):
-        self.__trie.add(word)
-
-    def correct_word(self, word):
-        lower_word = word.lower()
-        if self.__trie.search(lower_word):
-            return f"{word} - ok"
-
         corrections = []
-        for dict_word in self.__trie.get_words():
-            if self.__is_dam_lev_distance(lower_word, dict_word):
-                corrections.append(dict_word)
 
-        if corrections:
-            corrections.sort()
-            return f"{word} -> {', '.join(corrections)}"
-        else:
-            return f"{word} -?"
+        while stack:
+            node, prefix = stack.pop()
+            if node.is_word:
+                if self.__is_dam_lev_distance_one(word.lower(), prefix):
+                    corrections.append(prefix)
+
+            for child_suffix, child_node in node.children.items():
+                stack.append((child_node, prefix + child_suffix))
+
+        return corrections
 
     @staticmethod
-    def __is_dam_lev_distance(s1, s2):
+    def __is_dam_lev_distance_one(s1, s2):
         len_s1, len_s2 = len(s1), len(s2)
 
         if abs(len_s1 - len_s2) > 1:
@@ -90,28 +127,48 @@ class AutoCorrect:
 
         return True if prev_row[len_s2] == 1 else False
 
-    # @staticmethod
-    # def __dam_lev_distance(s1, s2):
-    #     len_s1, len_s2 = len(s1), len(s2)
-    #
-    #     d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
-    #
-    #     for i in range(len_s1 + 1):
-    #         d[i][0] = i
-    #     for j in range(len_s2 + 1):
-    #         d[0][j] = j
-    #
-    #     for i in range(1, len_s1 + 1):
-    #         for j in range(1, len_s2 + 1):
-    #             cost = 0 if s1[i - 1] == s2[j - 1] else 1
-    #             d[i][j] = min(d[i - 1][j] + 1,  # удаление
-    #                           d[i][j - 1] + 1,  # вставка
-    #                           d[i - 1][j - 1] + cost)  # замена
-    #
-    #             if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
-    #                 d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
-    #
-    #     return d[len_s1][len_s2]
+        # @staticmethod
+        # def __dam_lev_distance(s1, s2):
+        #     len_s1, len_s2 = len(s1), len(s2)
+        #
+        #     d = [[0] * (len_s2 + 1) for _ in range(len_s1 + 1)]
+        #
+        #     for i in range(len_s1 + 1):
+        #         d[i][0] = i
+        #     for j in range(len_s2 + 1):
+        #         d[0][j] = j
+        #
+        #     for i in range(1, len_s1 + 1):
+        #         for j in range(1, len_s2 + 1):
+        #             cost = 0 if s1[i - 1] == s2[j - 1] else 1
+        #             d[i][j] = min(d[i - 1][j] + 1,  # удаление
+        #                           d[i][j - 1] + 1,  # вставка
+        #                           d[i - 1][j - 1] + cost)  # замена
+        #
+        #             if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
+        #                 d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
+        #
+        #     return d[len_s1][len_s2]
+
+
+class AutoCorrect:
+    def __init__(self):
+        self.__radix = RadixTree()
+
+    def add_word(self, word):
+        self.__radix.add(word)
+
+    def correct_word(self, word):
+        if self.__radix.search(word):
+            return f"{word} - ok"
+
+        corrections = self.__radix.get_corrections(word)
+
+        if corrections:
+            corrections.sort()
+            return f"{word} -> {', '.join(corrections)}"
+        else:
+            return f"{word} -?"
 
 
 def main():
