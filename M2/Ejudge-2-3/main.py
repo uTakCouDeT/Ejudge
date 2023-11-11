6eb753eec4bbf55d176db2511b87ef87145a300d
uTakCouDeT <b20030021@gmail.com>
2023-11-11 04:30:24 +0300

    ]

--- M2/Ejudge-2-3/main.py
+++ M2/Ejudge-2-3/main.py
@@ -57,7 +57,7 @@ class AutoCorrect:
         corrections = []
         for dict_word in self.__trie.get_words():
             if abs(len(lower_word) - len(dict_word)) <= 1:
-                if dam_lev_distance(lower_word, dict_word) == 1:
+                if self.__dam_lev_distance(lower_word, dict_word) == 1:
                     corrections.append(dict_word)
 
         if corrections:
@@ -66,32 +66,32 @@ class AutoCorrect:
         else:
             return f"{word} -?"
 
+    @staticmethod
+    def __dam_lev_distance(s1, s2):
+        """
+        Рассчитывает расстояние Дамерау-Левенштейна между двумя строками.
+        """
+        len_s1 = len(s1)
+        len_s2 = len(s2)
 
-def dam_lev_distance(s1, s2):
-    """
-    Рассчитывает расстояние Дамерау-Левенштейна между двумя строками.
-    """
-    len_s1 = len(s1)
-    len_s2 = len(s2)
+        d = [[0 for _ in range(len_s2 + 1)] for _ in range(len_s1 + 1)]
 
-    d = [[0 for _ in range(len_s2 + 1)] for _ in range(len_s1 + 1)]
+        for i in range(len_s1 + 1):
+            d[i][0] = i
+        for j in range(len_s2 + 1):
+            d[0][j] = j
 
-    for i in range(len_s1 + 1):
-        d[i][0] = i
-    for j in range(len_s2 + 1):
-        d[0][j] = j
+        for i in range(1, len_s1 + 1):
+            for j in range(1, len_s2 + 1):
+                cost = 0 if s1[i - 1] == s2[j - 1] else 1
+                d[i][j] = min(d[i - 1][j] + 1,  # удаление
+                              d[i][j - 1] + 1,  # вставка
+                              d[i - 1][j - 1] + cost)  # замена
 
-    for i in range(1, len_s1 + 1):
-        for j in range(1, len_s2 + 1):
-            cost = 0 if s1[i - 1] == s2[j - 1] else 1
-            d[i][j] = min(d[i - 1][j] + 1,  # удаление
-                          d[i][j - 1] + 1,  # вставка
-                          d[i - 1][j - 1] + cost)  # замена
+                if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
+                    d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
 
-            if i > 1 and j > 1 and s1[i - 1] == s2[j - 2] and s1[i - 2] == s2[j - 1]:
-                d[i][j] = min(d[i][j], d[i - 2][j - 2] + cost)  # транспозиция
-
-    return d[len_s1][len_s2]
+        return d[len_s1][len_s2]
 
 
 def main():
