9983df7f7eec314c1a60eee210594300bd376ee1
uTakCouDeT <b20030021@gmail.com>
2023-12-23 13:44:26 +0300

    ъ

--- M3/Ejudge-3-3/main.py
+++ M3/Ejudge-3-3/main.py
@@ -2,16 +2,17 @@ import sys
 
 
 class Node:
-    """ Для повышения читаемости кода """
+    """ Для повышения читаемости кода запишем все предметы в объекты отдельного класса """
 
     def __init__(self, weight, cost, scaled_cost):
         self.weight = weight
         self.cost = cost
-        self.scaled_cost = scaled_cost
+        self.scaled_cost = scaled_cost  # Для приближенного решения задачи
+        # Записываем scaled_cost в отдельную переменную, тк исходные стоимости будут нужны при формировании ответа
 
 
 class Solution:
-    """ Чтобы программа не была заточена под вывод """
+    """ Класс, возвращаемый при решении задачи: Чтобы программа не была заточена под вывод """
 
     def __init__(self, total_weight, total_cost, selected_items):
         self.total_weight = total_weight
@@ -21,6 +22,7 @@ class Solution:
 
 class KnapsackProblem:
     def __init__(self, approximation, max_weight):
+        # Проверки на валидность входных данных (на всякий случай)
         if not 0 <= approximation <= 1:
             raise ValueError("Approximation coefficient must be between 0 and 1")
         if max_weight < 0:
@@ -31,36 +33,40 @@ class KnapsackProblem:
         self.max_cost = 0
 
     def add_item(self, weight, cost):
+        # Ну и тут тоже можно
         if weight < 0 or cost < 0:
             raise ValueError("Weight and value must be non-negative integers")
+        # Предметы, которые очевидно не влезут, можно не учитывать
         if weight <= self.max_weight:
             self.items.append(Node(weight, cost, 0))
             self.max_cost = max(self.max_cost, cost)
 
     def __calculate_scaled_costs(self):
-        scale = self.approximation * self.max_cost / len(self.items)
+        """ Масштабируем стоимости предметов для приближенного решения """
+        scale = self.approximation * self.max_cost / (len(self.items) * (1 + self.approximation))
         for item in self.items:
             item.scaled_cost = int(item.cost / scale)
 
     def __find_best_combination(self):
+        """ Находим лучшую комбинацию предметов, используя динамическое программирование (с учётом масштабирования) """
         self.__calculate_scaled_costs()
         dp = {0: (0, [])}
         for i, item in enumerate(self.items):
-            new_dp = dp.copy()
-            for total_cost, (total_weight, selected_items) in dp.items():
+            for total_cost, (total_weight, selected_items) in list(dp.items()):
                 new_weight = total_weight + item.weight
                 new_cost = total_cost + item.scaled_cost
-                if new_weight <= self.max_weight and (new_cost not in new_dp or new_dp[new_cost][0] > new_weight):
-                    new_dp[new_cost] = (new_weight, selected_items + [i])
-            dp = new_dp
+                if new_weight <= self.max_weight and (new_cost not in dp or dp[new_cost][0] > new_weight):
+                    dp[new_cost] = (new_weight, selected_items + [i])
         best_cost = max(dp.keys())
         return dp[best_cost]
 
     def get_solution(self):
+        # Если предметов нет, возвращаем пустое решение
         if not self.items:
             return Solution(0, 0, [])
+        # В остальных случаях считаем лучшую комбинацию
         best_weight, selected_items = self.__find_best_combination()
-        total_cost = sum(self.items[i].cost for i in selected_items)
+        total_cost = sum(self.items[i].cost for i in selected_items)  # Используем реальные стоимости
         return Solution(best_weight, total_cost, [i + 1 for i in selected_items])
 
 
